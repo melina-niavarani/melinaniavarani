7a9957185be0901ad6a612b0a4ed69005908e24e
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2025-09-26 18:21:27 +0330

    Update Experiences.vue

--- src/components/experience/Experiences.vue
+++ src/components/experience/Experiences.vue
@@ -32,7 +32,7 @@
                     rel="noreferrer"
                     class="group/link inline-flex items-baseline font-bold leading-tight text-neutral-100 hover:text-primary-300 focus-visible:text-primary-300 transition-colors duration-200"
                   >
-                    Full-stack Developer · <span class="gradient-text">Rahrosell (Online Bookstore)</span>
+                    Full-stack Developer · <span class="gradient-text">Rahrosell</span>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="inline-block h-5 w-5 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-2 translate-y-px" aria-hidden="true">
                       <path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path>
                     </svg>
@@ -144,7 +144,7 @@
                     rel="noreferrer"
                     class="group/link inline-flex items-baseline font-bold leading-tight text-neutral-100 hover:text-primary-300 focus-visible:text-primary-300 transition-colors duration-200"
                   >
-                    SEO & Digital Marketing Specialist · <span class="gradient-text">Dejpa Holding</span>
+                    SEO & Digital Marketing Specialist · <span class="gradient-text">Dejpa</span>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="inline-block h-5 w-5 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-2 translate-y-px" aria-hidden="true">
                       <path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path>
                     </svg>
